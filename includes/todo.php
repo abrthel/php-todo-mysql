4382d664624d9ed2b188e6935ed22f908708ab3e
Aaron Barthel <aaron.barthel@live.ca>
2020-10-16 12:03:32 -0600

    Add Todos DB abstraction

---
+++ includes/todo.php
@@ -0,0 +1,48 @@
+<?php
+
+class Todos {
+  public $conn;
+
+  function __construct($con)
+  {
+    $this->conn = $con;
+  }
+
+  function getAll() {
+    $query = "
+      SELECT entry_id, category_id, date_due, date_completed, description
+      FROM entries
+    ";
+
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $active = [];
+    $overdue = [];
+    $completed = [];
+
+    while($record = $result->fetch_assoc()) {
+      if ($record['date_completed']) {
+        array_push($completed, $record);
+      } else {
+
+        if($record['date_due'] < date('Y-m-d')) {
+          array_push($active, $record);
+        } else {
+          array_push($overdue, $record);
+        }
+      }
+    }
+
+    return [$active, $overdue, $completed];
+  }
+
+  function clear() {}
+
+  function add() {}
+
+  function complete() {}
+
+  function remove() {}
+}
